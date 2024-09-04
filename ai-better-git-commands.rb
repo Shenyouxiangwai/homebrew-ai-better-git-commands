8454cef5fd45655be00d10f358cf5ad1bce46006
TIANYANG <tianycq1@gmail.com>
2024-09-04 21:23:40 +0800

    feat: 拆分接口逻辑

--- ai-better-git-commands.rb
+++ ai-better-git-commands.rb
@@ -2,7 +2,7 @@ class AiBetterGitCommands < Formula
   desc "A script to automate git commit messages and changelogs"
   homepage "https://github.com/Shenyouxiangwai/homebrew-ai-better-git-commands"
   url "https://raw.githubusercontent.com/Shenyouxiangwai/homebrew-ai-better-git-commands/main/main.sh"
-  version "1.0.0"
+  version "1.1.0"
   sha256 "db2d25412aa418ba824c5f00a79f016f921c0fae36a74d8836175760be16c4e6" # 请替换为 main.sh 的实际 SHA256 值
 
   def install

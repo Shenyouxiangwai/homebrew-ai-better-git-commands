1631e42f8a47c8fe1f637b1771bc1b8aae7b0c33
TIANYANG <tianycq1@gmail.com>
2024-09-17 21:09:11 +0800

    feat: 拆分接口逻辑

--- ai-better-git-commands.rb
+++ ai-better-git-commands.rb
@@ -2,8 +2,8 @@ class AiBetterGitCommands < Formula
   desc "A script to automate git commit messages and changelogs"
   homepage "https://github.com/Shenyouxiangwai/homebrew-ai-better-git-commands"
   url "https://raw.githubusercontent.com/Shenyouxiangwai/homebrew-ai-better-git-commands/main/ait.sh"
-  version "1.1.0"
-  sha256 "22958298b4f1176e96f106fd588a714c5092a2db54c564b65d35f75d3315ecf9"
+  version "1.1.1"
+  sha256 "2902a79ec6c6c8837520b472ba2bd0edfe5f2d33720e8ac0371d3a4a2394c395"
 
   def install
     bin.install "ait.sh" => "ait"

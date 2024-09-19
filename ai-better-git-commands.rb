b627ece755a26eb2176c0b89b9b94b4905ff8578
TIANYANG <tianycq1@gmail.com>
2024-09-19 18:04:28 +0800

    feat: 更新版本号和SHA256校验值

--- ai-better-git-commands.rb
+++ ai-better-git-commands.rb
@@ -2,8 +2,8 @@ class AiBetterGitCommands < Formula
   desc "A script to automate git commit messages and changelogs"
   homepage "https://github.com/Shenyouxiangwai/homebrew-ai-better-git-commands"
   url "https://raw.githubusercontent.com/Shenyouxiangwai/homebrew-ai-better-git-commands/main/ait.sh"
-  version "1.1.1"
-  sha256 "ac131fcbdd6a9cd6b3c8f7c0d0dab356b9297a9d634124ba7534f739a8d189a9"
+  version "1.1.2"
+  sha256 "0a511c4441c1549d4aaf340b539d52c17f143bb3202ddd6e81b5616cdbf7c350"
 
   def install
     bin.install "ait.sh" => "ait"
